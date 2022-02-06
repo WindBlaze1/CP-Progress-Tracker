6642bc361b028ca2f1ea973368ff85cbcc626c44
Harit <haritmohanhm@gmail.com>
2022-02-06 20:54:37 +0530

    Deployment

--- project01/settings.py
+++ project01/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*boi*z*q(bso!wc510ez274j(myv5!lysknveb-nnyei(&+z^h'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['cp-progress-tracker.herokuapp.com', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
